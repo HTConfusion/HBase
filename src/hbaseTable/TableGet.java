1b089ea424dfb39dc1a0eb431769f0cba0008ffd
HTConfusion <710482043@qq.com>
2018-02-02 20:21:14 +0800

    hbase -> get scan filter

--- src/hbaseTable/TableGet.java
+++ src/hbaseTable/TableGet.java
@@ -27,65 +27,65 @@ public class TableGet {
         get.addFamily(Bytes.toBytes("fam1"));
         Result result = table.get(get);
         //------------------------------------------------------
-//        for (Cell c : result.rawCells()) {//result.listCells();
-//            //row-4/fam1:col-1/1517478166779/Put/vlen=7/seqid=0
-//            System.out.println(c);
-//            //可直接输出时间戳
-//            System.out.println(c.getTimestamp());
-//            //列长度--列起始位置
-//            System.out.println(c.getQualifierLength()+"--"+c.getQualifierOffset());
-//        }
+        for (Cell c : result.rawCells()) {//result.listCells();
+            //row-4/fam1:col-1/1517478166779/Put/vlen=7/seqid=0
+            System.out.println(c);
+            //可直接输出时间戳
+            System.out.println(c.getTimestamp());
+            //列长度--列起始位置
+            System.out.println(c.getQualifierLength()+"--"+c.getQualifierOffset());
+        }
         /**
          * 取值
          */
         //CellUtil--------------------------------------------------
-//        for(Cell c:result.rawCells()){
-//            //获取行键，列族，列，值
-//            System.out.println(new String(CellUtil.cloneRow(c)));
-//            System.out.println(new String(CellUtil.cloneFamily(c)));
-//            System.out.println(new String(CellUtil.cloneQualifier(c)));
-//            System.out.println(new String(CellUtil.cloneValue(c)));
-//        }
+        for(Cell c:result.rawCells()){
+            //获取行键，列族，列，值
+            System.out.println(new String(CellUtil.cloneRow(c)));
+            System.out.println(new String(CellUtil.cloneFamily(c)));
+            System.out.println(new String(CellUtil.cloneQualifier(c)));
+            System.out.println(new String(CellUtil.cloneValue(c)));
+        }
         //Cell------------------------------------------------------
-//        for (Cell c : result.rawCells()) {
-//            //过期--但可用
-//            System.out.println(Bytes.toString(c.getRow()));
-//            System.out.println(Bytes.toString(c.getFamily()));
-//            System.out.println(Bytes.toString(c.getQualifier()));
-//            System.out.println(Bytes.toString(c.getValue()));
-//            System.out.println(c.getTimestamp());
-//        }
+        for (Cell c : result.rawCells()) {
+            //过期--但可用
+            System.out.println(Bytes.toString(c.getRow()));
+            System.out.println(Bytes.toString(c.getFamily()));
+            System.out.println(Bytes.toString(c.getQualifier()));
+            System.out.println(Bytes.toString(c.getValue()));
+            System.out.println(c.getTimestamp());
+        }
         //KeyValue--------------------------------------------------
-//        for (KeyValue k : result.raw()) {//Cell c : result.raw()
-//            //过期--但可用
-//            System.out.println(new String(k.getRow()));
-//            System.out.println(new String(k.getFamily()));
-//            System.out.println(new String(k.getQualifier()));
-//            System.out.println(k.getTimestamp());
-//            System.out.println(new String(k.getValue()));
-//        }
+        for (KeyValue k : result.raw()) {//Cell c : result.raw()
+            //过期--但可用
+            System.out.println(new String(k.getRow()));
+            System.out.println(new String(k.getFamily()));
+            System.out.println(new String(k.getQualifier()));
+            System.out.println(k.getTimestamp());
+            System.out.println(new String(k.getValue()));
+        }
         //Cell截串---------------------------------------------------
-//        for (Cell c : result.rawCells()) {
-//            //获取一行
-//            //       row-4fam1col-1  aP�@�value41
-//            String row = new String(c.getValueArray());
-//            System.out.println(row);
-//            //获取偏移量，获取长度
-//            //行键
-//            System.out.println(row.substring(c.getRowOffset(),
-//                    c.getRowOffset() + c.getRowLength()));
-//            //列族
-//            System.out.println(row.substring(c.getFamilyOffset(),
-//                    c.getFamilyOffset() + c.getFamilyLength()));
-//            //列名
-//            System.out.println(row.substring(c.getQualifierOffset(),
-//                    c.getQualifierOffset() + c.getQualifierLength()));
-//            //时间戳
-//            System.out.println(c.getTimestamp());
-//            //值
-//            System.out.println(row.substring(c.getValueOffset(),
-//                    c.getValueOffset() + c.getValueLength()));
-//        }
+        for (Cell c : result.rawCells()) {
+            //获取一行
+            //       row-4fam1col-1  aP�@�value41
+            String row = new String(c.getValueArray());
+            System.out.println(row);
+            //获取偏移量，获取长度
+            //行键
+            System.out.println(row.substring(c.getRowOffset(),
+                    c.getRowOffset() + c.getRowLength()));
+            //列族
+            System.out.println(row.substring(c.getFamilyOffset(),
+                    c.getFamilyOffset() + c.getFamilyLength()));
+            //列名
+            System.out.println(row.substring(c.getQualifierOffset(),
+                    c.getQualifierOffset() + c.getQualifierLength()));
+            //时间戳
+            System.out.println(c.getTimestamp());
+            //值
+            System.out.println(row.substring(c.getValueOffset(),
+                    c.getValueOffset() + c.getValueLength()));
+        }
         //多版本查询-----------------------------------------
         Get get1 = new Get(Bytes.toBytes("row-1"));
         //可不限定列族
--- src/hbaseTable/TableScan.java
+++ src/hbaseTable/TableScan.java
@@ -1,13 +1,12 @@
 package hbaseTable;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.BinaryComparator;
-import org.apache.hadoop.hbase.filter.ByteArrayComparable;
-import org.apache.hadoop.hbase.filter.CompareFilter;
-import org.apache.hadoop.hbase.filter.RowFilter;
+import org.apache.hadoop.hbase.filter.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
@@ -25,28 +24,50 @@ public class TableScan {
         TableName tableName = TableName.valueOf("tablename");
         Table table = conn.getTable(tableName);
         //1---------------------------------------------------
+//        //输入不存在列也可比较运行，容错率高
 //        Scan scan = new Scan();
 //        //左闭右开
-//        scan.setStartRow(Bytes.toBytes("row-4"));
+//        scan.setStartRow(Bytes.toBytes("row-1"));
 //        scan.setStopRow(Bytes.toBytes("row-8"));
+//        ResultScanner results = table.getScanner(scan);
+//        for(Result r:results){
+//            System.out.println(r);
+//        }
         //2---------------------------------------------------
+//        //构造器设定起止位置
 //        Scan scan = new Scan(Bytes.toBytes("row-1"),Bytes.toBytes("row-8"));
 //        ResultScanner result = table.getScanner(scan);
 //        for (Result r : result) {
 //            System.out.println(r);
 //        }
         //3.过滤器---------------------------------------------
-        Scan scan = new Scan();
-        //第一参数：枚举参数--选择等于或大于等对比类型  第二参数：ByteArrayComparable的子类--选择阈值或正则等对比方法
-        CompareFilter filter = new RowFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(Bytes.toBytes("row-4")));
-        //向Scan植入过滤器
-        scan.setFilter(filter);
-        ResultScanner result = table.getScanner(scan);
-        for (Result r : result) {
-            System.out.println(r);
-        }
+//        Scan scan = new Scan();
+//        //第一参数：枚举参数--选择等于或大于等对比类型  第二参数：ByteArrayComparable的子类--选择阈值或正则等对比方法
+//        //行过滤器
+//        //CompareFilter filter = new RowFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(Bytes.toBytes("row-3")));
+//        //值过滤器
+//        CompareFilter filter = new ValueFilter(CompareFilter.CompareOp.EQUAL, new SubstringComparator("1"));
+//        //        //向Scan植入过滤器
+//        scan.setFilter(filter);
+//        ResultScanner result = table.getScanner(scan);
+//        for (Result r : result) {
+//            System.out.println(r);
+//            for (Cell c : r.rawCells()) {
+//                System.out.println(new String(CellUtil.cloneValue(c)));
+//            }
+//        }
         //4.多版本查询-----------------------------------------
-
+        Scan scan = new Scan(Bytes.toBytes("row-1"), Bytes.toBytes("row-2"));
+        scan.setMaxVersions();
+        ResultScanner results = table.getScanner(scan);
+        for (Result r : results) {
+            System.out.println(r);
+            for (Cell c : r.listCells()) {
+                System.out.println(new String(CellUtil.cloneFamily(c)));
+                System.out.println(new String(CellUtil.cloneQualifier(c)));
+                System.out.println(new String(CellUtil.cloneValue(c)));
+            }
+        }
         table.close();
         conn.close();
     }
